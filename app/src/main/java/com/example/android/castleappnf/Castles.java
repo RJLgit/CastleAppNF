3a215a5feb277755b8228a1f262c703bcf614c80
Rich <r.j.lewindon@hotmail.co.uk>
2020-05-07 15:26:30 +0100

    Added dummy data and files for castles

---
+++ app/src/main/java/com/example/android/castleappnf/Castles.java
@@ -0,0 +1,68 @@
+package com.example.android.castleappnf;
+
+public class Castles {
+    private String name;
+    private String operator;
+    //Need to add time and geolocation in future
+    private int rating;
+    private String history;
+    private int image;
+    private int audio;
+
+    public Castles(String name, String operator, int rating, String history, int image, int audio) {
+        this.name = name;
+        this.operator = operator;
+        this.rating = rating;
+        this.history = history;
+        this.image = image;
+        this.audio = audio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getHistory() {
+        return history;
+    }
+
+    public void setHistory(String history) {
+        this.history = history;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public int getAudio() {
+        return audio;
+    }
+
+    public void setAudio(int audio) {
+        this.audio = audio;
+    }
+}
