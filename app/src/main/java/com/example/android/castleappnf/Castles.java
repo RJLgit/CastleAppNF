4625767b43f3d06931fa8d60348679eac7ecbfaa
Rich <r.j.lewindon@hotmail.co.uk>
2020-05-11 07:22:37 +0100

    Added Long and Lat to Castles class

--- app/src/main/java/com/example/android/castleappnf/Castles.java
+++ app/src/main/java/com/example/android/castleappnf/Castles.java
@@ -14,8 +14,10 @@ public class Castles implements Serializable {
     private int audio;
     //Hard coded until calculated from location data and phone location
     private int distance;
+    private double longdi;
+    private double lat;
 
-    public Castles(String name, String operator, int rating, String history, int image, int audio) {
+    public Castles(String name, String operator, int rating, String history, int image, int audio, double latitude, double longditude) {
         this.name = name;
         this.operator = operator;
         this.rating = rating;
@@ -23,6 +25,8 @@ public class Castles implements Serializable {
         this.image = image;
         this.audio = audio;
         this.distance = 45;
+        this.lat = latitude;
+        this.longdi = longditude;
     }
 
     public String getName() {
@@ -79,5 +83,21 @@ public class Castles implements Serializable {
 
     public void setDistance(int distance) {
         this.distance = distance;
+    }
+
+    public double getLongdi() {
+        return longdi;
+    }
+
+    public void setLongdi(double longdi) {
+        this.longdi = longdi;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
     }
 }
