0d015f2d0660ff44ed9c2f8ba33639a4adccb61a
Rich <r.j.lewindon@hotmail.co.uk>
2020-05-17 10:25:39 +0100

    Created arrays for other castle pics

--- app/src/main/java/com/example/android/castleappnf/DummyData.java
+++ app/src/main/java/com/example/android/castleappnf/DummyData.java
@@ -19,17 +19,26 @@ public class DummyData {
         Castles caenarven;
 
         int[] alnPics = {R.drawable.alnwick_castle, R.drawable.alnwickpictwo, R.drawable.alnwickpicthree};
+        int[] walkPics = {R.drawable.walkworth_castle};
+        int[] warwickPics = {R.drawable.warwick_castle};
+        int[] dunstanPics = {R.drawable.dunstan_castle};
+        int[] rabyPics = {R.drawable.raby_castle};
+        int[] berkPics = {R.drawable.berk_castle};
+        int[] caenPics = {R.drawable.caenarven_castle};
+        int[] banPics = {R.drawable.banburgh_castle};
+        int[] risPics = {R.drawable.castle_rising};
+        int[] dovPics = {R.drawable.dover_castle};
 
-        warkworth = new Castles("Warkworth Castle", "English Heritage", 5, context.getString(R.string.warkworth_History), alnPics, R.raw.canon_in_d, -1.6118, 55.3452, "https://www.english-heritage.org.uk/visit/places/warkworth-castle-and-hermitage/", "https://www.english-heritage.org.uk/visit/places/warkworth-castle-and-hermitage/prices-and-opening-times/");
-        dunstan = new Castles("Dunstanburgh Castle", "English Heritage", 3, context.getString(R.string.dunstan_History), alnPics, R.raw.four_seasons_spring, -1.5950, 55.4894, "https://www.english-heritage.org.uk/visit/places/dunstanburgh-castle/", "https://www.english-heritage.org.uk/visit/places/dunstanburgh-castle/prices-and-opening-times/");
-        rising = new Castles("Castle Rising", "Castle rising", 45, context.getString(R.string.rising_History), alnPics, R.raw.canon_in_d, 0.4704, 52.7981, "http://www.castlerising.co.uk/", "http://www.castlerising.co.uk/visit-us/");
-        berkhamsted = new Castles("Berkhamsted Castle", "English Heritage", 2, context.getString(R.string.berk_History), alnPics, R.raw.four_seasons_spring, -0.5591, 51.7639, "https://www.english-heritage.org.uk/visit/places/berkhamsted-castle/", "https://www.english-heritage.org.uk/visit/places/berkhamsted-castle/");
-        warwick = new Castles("Warwick Castle", "Warwick Castle", 5, context.getString(R.string.warwick_History), alnPics, R.raw.canon_in_d, -1.5852, 52.2797, "https://www.warwick-castle.com/", "https://www.warwick-castle.com/tickets-passes/day-tickets/castle-tickets/");
-        banbury = new Castles("Bamburgh Castle", "Bamburgh Castle", 1, context.getString(R.string.banbury_History), alnPics, R.raw.four_seasons_spring, -1.7099, 55.6090, "https://www.bamburghcastle.com/", "https://www.bamburghcastle.com/visit-us/");
+        warkworth = new Castles("Warkworth Castle", "English Heritage", 5, context.getString(R.string.warkworth_History), walkPics, R.raw.canon_in_d, -1.6118, 55.3452, "https://www.english-heritage.org.uk/visit/places/warkworth-castle-and-hermitage/", "https://www.english-heritage.org.uk/visit/places/warkworth-castle-and-hermitage/prices-and-opening-times/");
+        dunstan = new Castles("Dunstanburgh Castle", "English Heritage", 3, context.getString(R.string.dunstan_History), dunstanPics, R.raw.four_seasons_spring, -1.5950, 55.4894, "https://www.english-heritage.org.uk/visit/places/dunstanburgh-castle/", "https://www.english-heritage.org.uk/visit/places/dunstanburgh-castle/prices-and-opening-times/");
+        rising = new Castles("Castle Rising", "Castle rising", 45, context.getString(R.string.rising_History), risPics, R.raw.canon_in_d, 0.4704, 52.7981, "http://www.castlerising.co.uk/", "http://www.castlerising.co.uk/visit-us/");
+        berkhamsted = new Castles("Berkhamsted Castle", "English Heritage", 2, context.getString(R.string.berk_History), berkPics, R.raw.four_seasons_spring, -0.5591, 51.7639, "https://www.english-heritage.org.uk/visit/places/berkhamsted-castle/", "https://www.english-heritage.org.uk/visit/places/berkhamsted-castle/");
+        warwick = new Castles("Warwick Castle", "Warwick Castle", 5, context.getString(R.string.warwick_History), warwickPics, R.raw.canon_in_d, -1.5852, 52.2797, "https://www.warwick-castle.com/", "https://www.warwick-castle.com/tickets-passes/day-tickets/castle-tickets/");
+        banbury = new Castles("Bamburgh Castle", "Bamburgh Castle", 1, context.getString(R.string.banbury_History), banPics, R.raw.four_seasons_spring, -1.7099, 55.6090, "https://www.bamburghcastle.com/", "https://www.bamburghcastle.com/visit-us/");
         alnwick = new Castles("Alnwick Castle", "Alnwick Castle", 3, context.getString(R.string.alnwick_History), alnPics, R.raw.canon_in_d, -1.7059, 55.4156, "https://www.alnwickcastle.com/", "https://www.alnwickcastle.com/opening-times-and-ticket-prices");
-        dover = new Castles("Dover Castle", "English Heritage", 4, context.getString(R.string.dover_History), alnPics, R.raw.canon_in_d, 1.3234, 51.1288, "https://www.english-heritage.org.uk/visit/places/dover-castle/", "https://www.english-heritage.org.uk/visit/places/dover-castle/prices-and-opening-times/");
-        raby = new Castles("Raby Castle", "Raby Castle", 2, context.getString(R.string.raby_History), alnPics, R.raw.canon_in_d, -1.8020, 54.5916, "https://www.raby.co.uk/raby-castle/", "https://www.raby.co.uk/raby-castle/your-visit/plan-your-day/opening-times-prices/");
-        caenarven = new Castles("Caernarfon Castle", "Cadw", 5, context.getString(R.string.caen_History), alnPics, R.raw.canon_in_d, -4.2769, 53.1391, "https://cadw.gov.wales/visit/places-to-visit/caernarfon-castle", "https://cadw.gov.wales/visit/places-to-visit/caernarfon-castle#opening-times");
+        dover = new Castles("Dover Castle", "English Heritage", 4, context.getString(R.string.dover_History), dovPics, R.raw.canon_in_d, 1.3234, 51.1288, "https://www.english-heritage.org.uk/visit/places/dover-castle/", "https://www.english-heritage.org.uk/visit/places/dover-castle/prices-and-opening-times/");
+        raby = new Castles("Raby Castle", "Raby Castle", 2, context.getString(R.string.raby_History), rabyPics, R.raw.canon_in_d, -1.8020, 54.5916, "https://www.raby.co.uk/raby-castle/", "https://www.raby.co.uk/raby-castle/your-visit/plan-your-day/opening-times-prices/");
+        caenarven = new Castles("Caernarfon Castle", "Cadw", 5, context.getString(R.string.caen_History), caenPics, R.raw.canon_in_d, -4.2769, 53.1391, "https://cadw.gov.wales/visit/places-to-visit/caernarfon-castle", "https://cadw.gov.wales/visit/places-to-visit/caernarfon-castle#opening-times");
         result.add(alnwick);
         result.add(banbury);
         result.add(berkhamsted);
