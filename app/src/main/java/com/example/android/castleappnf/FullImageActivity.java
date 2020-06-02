cb8def11f30bfebd2fb04520add8735c7e7c1a47
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-02 13:36:58 +0100

    Created new activity to hold full images

---
+++ app/src/main/java/com/example/android/castleappnf/FullImageActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.castleappnf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class FullImageActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_image);
+        imageView = findViewById(R.id.fullImageView);
+        
+    }
+}
