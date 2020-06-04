38a49f621cf5dfa31ba4d9bdacbd2b8f70ad4dd3
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-04 10:30:19 +0100

    Implemented method to create pending intent and bitmap icon resource in notification class

--- app/src/main/java/com/example/android/castleappnf/NotificationUtils.java
+++ app/src/main/java/com/example/android/castleappnf/NotificationUtils.java
@@ -1,5 +1,12 @@
 package com.example.android.castleappnf;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 public class NotificationUtils {
     private static final int CASTLE_NOTIFICATION_ID = 1246;
 
@@ -7,5 +14,15 @@ public class NotificationUtils {
 
     private static final String CASTLE_NOTIFICATION_CHANNEL_ID = "castle_notification_channel";
 
+    private static PendingIntent getPendingIntent(Context context) {
+        Intent startActivityIntent = new Intent(context, MainActivity.class);
+        return PendingIntent.getActivity(context, CASTLE_PENDING_INTENT_ID, startActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
 
+    private static Bitmap getNotificationIcon(Context context) {
+        Resources res = context.getResources();
+        Bitmap myIcon = BitmapFactory.decodeResource(res, R.drawable.castle);
+        return myIcon;
+    }
+    
 }
