c26253b40c2a6522c4815764e56af6acc3ba4ebe
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-04 10:17:20 +0100

    Added notification utils class and constants

---
+++ app/src/main/java/com/example/android/castleappnf/NotificationUtils.java
@@ -0,0 +1,11 @@
+package com.example.android.castleappnf;
+
+public class NotificationUtils {
+    private static final int CASTLE_NOTIFICATION_ID = 1246;
+
+    private static final int CASTLE_PENDING_INTENT_ID = 6542;
+
+    private static final String CASTLE_NOTIFICATION_CHANNEL_ID = "castle_notification_channel";
+
+    
+}
