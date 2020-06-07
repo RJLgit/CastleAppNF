752921a17de3226095936b3ee52a003ab6ec0a3b
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-07 09:52:40 +0100

    Commented settings fragment class

--- app/src/main/java/com/example/android/castleappnf/SettingsFragment.java
+++ app/src/main/java/com/example/android/castleappnf/SettingsFragment.java
@@ -1,9 +1,8 @@
 package com.example.android.castleappnf;
 
 import android.os.Bundle;
-
 import androidx.preference.PreferenceFragmentCompat;
-
+//Settings Fragment class which defines the relevent XML file for the settings
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
