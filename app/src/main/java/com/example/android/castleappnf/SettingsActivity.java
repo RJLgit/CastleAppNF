4ac547cf6130e26f3ac9bebbe1ae8974ef11c9a2
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-07 09:51:46 +0100

    Tidyed up SettingsActivity

--- app/src/main/java/com/example/android/castleappnf/SettingsActivity.java
+++ app/src/main/java/com/example/android/castleappnf/SettingsActivity.java
@@ -10,33 +10,29 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    //Ui variables
     Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        //Sets the toolbar up
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Settings");
-
-
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
     }
 
+    //Method which navigates back to the MainActivity when the "back" option menu is clicked on the toolbar.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            
+            //Navigates up to MainActivity using this method
             NavUtils.navigateUpFromSameTask(this);
             overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
