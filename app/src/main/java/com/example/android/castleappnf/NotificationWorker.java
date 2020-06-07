f3041e8b774c79ca9070c3468a58d4c27441ac91
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-07 09:30:54 +0100

    Commented NotificationWorker class

--- app/src/main/java/com/example/android/castleappnf/NotificationWorker.java
+++ app/src/main/java/com/example/android/castleappnf/NotificationWorker.java
@@ -8,6 +8,8 @@ import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+//Custom Worker class which creates the notification. This class is used to create the periodic worker request in the Main activity class.
+//This class handles all the background work to handle the posting of notifications even when the app isn't running
 public class NotificationWorker extends Worker {
 
     Context context;
@@ -20,6 +22,7 @@ public class NotificationWorker extends Worker {
         Log.d(TAG, "NotificationWorker: ");
     }
 
+    //This method defines the actions that the Worker object needs to carry out
     @NonNull
     @Override
     public Result doWork() {
