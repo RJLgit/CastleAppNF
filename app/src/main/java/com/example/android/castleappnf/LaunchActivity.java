10972084de3d62dc47f67a71b60a8c34b627bf11
Rich <r.j.lewindon@hotmail.co.uk>
2020-07-23 08:44:54 +0100

    Got API key in app

--- app/src/main/java/com/example/android/castleappnf/LaunchActivity.java
+++ app/src/main/java/com/example/android/castleappnf/LaunchActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LaunchActivity extends AppCompatActivity {
     private Button mainButton;
@@ -24,5 +25,6 @@ public class LaunchActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Toast.makeText(this, BuildConfig.GOOGLE_MAPS_API_KEY, Toast.LENGTH_SHORT).show();
     }
 }
