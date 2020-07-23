71111bb1ee0c67ad1c576b7f9ab61a6b39151ade
Rich <r.j.lewindon@hotmail.co.uk>
2020-07-23 08:00:51 +0100

    Added new host activity and made it launch activity and parent of main activity

---
+++ app/src/main/java/com/example/android/castleappnf/LaunchActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.castleappnf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
