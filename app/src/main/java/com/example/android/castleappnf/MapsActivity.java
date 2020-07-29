111618298d56eb9b77c6e0c2be3f5eee5ab33dac
Rich <r.j.lewindon@hotmail.co.uk>
2020-07-29 07:23:57 +0100

    Tidyed up maps activity

--- app/src/main/java/com/example/android/castleappnf/MapsActivity.java
+++ app/src/main/java/com/example/android/castleappnf/MapsActivity.java
@@ -41,44 +41,50 @@ import com.google.android.material.snackbar.Snackbar;
 import java.util.ArrayList;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
-
+    //Constants for Permission requests
     private static final int REQUEST_LOCATION_PERMISSION = 12;
     private static final int PERMISSIONS_REQUEST_ENABLE_GPS = 13;
+    //Map and location variables
     private GoogleMap mMap;
-    private Toolbar toolbar;
-    private CoordinatorLayout coordinatorLayout;
-    ArrayList<Castles> myCastles;
     boolean locationPermissionGranted;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private Location lastKnownLocation;
-    private static final String TAG = "MapsActivity";
     private SupportMapFragment mapFragment;
+    //Ui variables
+    private Toolbar toolbar;
+    private CoordinatorLayout coordinatorLayout;
+    //Array list of all the castles to populate the map with
+    ArrayList<Castles> myCastles;
+
+    private static final String TAG = "MapsActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
+        //Get all the castles in the app
         myCastles = DummyData.generateAndReturnDataAZ(this);
 
         coordinatorLayout = findViewById(R.id.cordlay);
-
+        //Set up the toolbar
         toolbar = findViewById(R.id.toolbar);
-        toolbar.setSubtitle("Click a castle to see more details");
+        toolbar.setSubtitle(R.string.maps_activity_toolbar_subtitle);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        //Checks if GPS is enabled on the device
         if (isMapsEnabled()) {
+            //If so then trigger the onMapReady callback
             mapFragment.getMapAsync(this);
         }
     }
-
+    //Checks if the app has GPS enabled, if not it builds an alert to request it
     public boolean isMapsEnabled(){
         final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
-        Log.d(TAG, "isMapsEnabled: ");
         if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
             buildAlertMessageNoGps();
             return false;
@@ -88,7 +94,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     //Method builds an Alert which allows the user to enable GPS permission.
     private void buildAlertMessageNoGps() {
-        Log.d(TAG, "buildAlertMessageNoGps: ");
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("This application requires GPS to work properly, do you want to enable it?")
                 .setCancelable(false)
@@ -102,7 +107,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         alert.show();
     }
 
-    //On activity result called when the GPS permission is granted. This then checks to see if the location permission is granted. If not it requests that. If so then it continues with the app function
+    //On activity result called when the GPS permission is granted. This then checks to see if the GPS permission is granted. If so then it continues with the app function
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -125,7 +130,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        //Method to handle the location persmission and operations
         enableMyLocation();
 
         populateMap();
@@ -133,35 +138,37 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void enableMyLocation() {
+        //First checks for location permission
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
         == PackageManager.PERMISSION_GRANTED) {
+            //If granted then sets the location enabled to true and sets button to allow user to zoom on location
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            //Sets the boolean to true to track location permission
             locationPermissionGranted = true;
             fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
             try {
+                //Gets the last location
                 Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
                 locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
                     @Override
                     public void onComplete(@NonNull Task<Location> task) {
                         if (task.isSuccessful()) {
-
                             // Set the map's camera position to the current location of the device.
-                            Log.d(TAG, "onComplete: success");
                             lastKnownLocation = task.getResult();
                             if (lastKnownLocation == null) {
+                                //If last location does not exist then centres camera on York
                                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
                                         new LatLng(53.9600,
                                                 -1.0873), 8));
-                                Log.d(TAG, "onComplete: last location null");
                             } else {
-                                Log.d(TAG, "onComplete: location not null" + lastKnownLocation);
+                                //Else centres camera on last location
                                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
                                         new LatLng(lastKnownLocation.getLatitude(),
                                                 lastKnownLocation.getLongitude()), 8));
                             }
                         } else {
-                            Log.d(TAG, "onComplete: task not success");
+                            //If task not successful then centres location york and sets the location button enabled to false
                             LatLng uk = new LatLng(53.9600, -1.0873);
                             float zoom = 5;
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uk, zoom));
@@ -173,6 +180,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 Log.e("Exception: %s", e.getMessage(), e);
             }
         } else {
+            //If permission not granted then shows dialog if rationale required
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
                 new AlertDialog.Builder(this).setTitle(getString(R.string.permission_rationale_title))
@@ -195,22 +203,22 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         })
                         .create().show();
             } else {
+                //If rational not required then permission requested
                 ActivityCompat.requestPermissions(this,
                         new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                         REQUEST_LOCATION_PERMISSION);
             }
         }
     }
-
+    //Callback method for when permission request result is received
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        Log.d(TAG, "onRequestPermissionsResult: general");
         switch (requestCode) {
             case REQUEST_LOCATION_PERMISSION:
                 if (grantResults.length > 0
                         && grantResults[0]
                         == PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "onRequestPermissionsResult: ");
+                    //Calls enable my location method if permission granted
                     enableMyLocation();
                     break;
                 } else {
@@ -229,30 +237,32 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
         }
     }
-
+    //This method iterates through the Castles objects and populates the maps with markers
+    //Each marker has the title of the castle and who it is operated by
     private void populateMap() {
         for (Castles c : myCastles) {
             LatLng pos = new LatLng(c.getLat(), c.getLongdi());
             mMap.addMarker(new MarkerOptions().position(pos)
-            .title(c.getName()).snippet("Operated by: " + c.getOperator()));
+            .title(c.getName()).snippet(getString(R.string.maps_marker_snippet, c.getOperator())));
         }
     }
-
+    //When the marker is clicked a snackbar shows and allows the user to see the details page for the castle
     @Override
     public boolean onMarkerClick(final Marker marker) {
         marker.showInfoWindow();
         final Snackbar snackBar = Snackbar.make(coordinatorLayout, marker.getTitle(), Snackbar.LENGTH_INDEFINITE);
         snackBar.setActionTextColor(getResources().getColor(R.color.colorAction));
-        snackBar.setAction("See details", new View.OnClickListener() {
+        snackBar.setAction(R.string.snackbar_action_text, new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Sets Castle object to first item as placeholder to avoid null errors
                 Castles clicked = myCastles.get(0);
                 for (Castles c : myCastles) {
                     if (c.getName().equals(marker.getTitle())) {
+                        //Overrides Castles with correct castle
                         clicked = c;
                     }
                 }
-
                 Intent intent = new Intent(MapsActivity.this, DetailsActivity.class);
                 intent.putExtra("Castle", clicked);
                 startActivity(intent);
